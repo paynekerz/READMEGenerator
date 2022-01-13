3152fcc704d3b2b7e54dbdd5c0e99320420304b4
Payne Kerz <paynekerz@gmail.com>
2022-01-13 11:34:04 -0700

    tweaks

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,9 @@
-// TODO: Include packages needed for this application
+
 const markdown = require("./utils/generateMarkdown");
 const inquirer = require("inquirer");
 const fs = require("fs");
 const path = require("path")
 
-// TODO: Create an array of questions for user input
 const questions = [
     {
         type: "input",
@@ -59,12 +58,12 @@ const questions = [
     },
 ];
 
-// TODO: Create a function to write README file
+
 function writeToFile(fileName, data) {
     return fs.writeFileSync(path.join(__dirname, fileName), data)
 }
 
-// TODO: Create a function to initialize app
+
 function init() {
     inquirer.prompt(questions)
     .then((userInput) => {
